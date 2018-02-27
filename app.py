2cda556d22e942467687ae65431b55bf6f466ef5
Xavier Vani-Charron <xavier.vani@gmail.com>
2018-02-26 20:23:23 -0500

    useful code

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
+#importing various modules
 import sys
 import random
 import os
 
 
-def dirtFunc():
-    this = "This is a string"
-    return this
+#function definition uses the "def" keyword
+def myFunc(param):
+    return param
 
-print(dirtFunc())
+#global variables put _G when making it global
+numba_G = 5
 
-#added some comment
-quote = "\" "
+def foo(num):
+    #use global keyword to modify the global "numba"
+    global numba_G
+    numba_G = num
 
-grocery_list = ['Juice', 5, 'Tomatoes', 'Potatoes']
+print(numba_G)
+foo(10)
+print(numba_G)
 
-print(grocery_list[0])
+#classes the __init__ means initialization
+#the self keyword is like the this keyword
+#for default constructor your need to put data="default" like this
+class TestClass:
+        def __init__(self, data="default", more="default"):
+            self.data = data
+            self.more = more
+
+
+test_obj = TestClass("This is the data","more data here")
+
+print(test_obj.data+" "+test_obj.more)
+
+test_obj2 = TestClass()
+
+print(test_obj2.data+" "+test_obj2.more)
